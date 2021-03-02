fcf40a52f2a7def4b98629a11a9674bac95defd2
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2021-03-02 16:58:25 -0500

    upload database.py

---
+++ mood-journal/database.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Name: Ezriel Ciriaco
+      Zachary Dulac
+"""
+
+import redis
+r = redis.Redis(host = "redis-17190.c99.us-east-1-4.ec2.cloud.redislabs.com", port = "17190", password = "LDMwbQfGPmB8RRyDt3X4ZujdF8JguKAi")
