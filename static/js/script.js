c4fa346edad3e1bb2b6e91925b70119de571871a
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2021-04-01 15:42:10 -0400

    Update files

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
-(function() {
-
-}).call(this);
+function revealForm() {
+    var x = document.getElementsByClassName("hiddenForm");
+    for (var i = 0; i < x.length; i++) {
+        if (x[i].style.display === "none") {
+            x[i].style.display = "block";
+        } else {
+            x[i].style.display = "none";
+        }
+    }
+}
