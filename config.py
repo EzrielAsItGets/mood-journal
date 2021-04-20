bc1048e168ff5df01781c07d30308ca939780520
Ezriel J. Ciriaco <ej.ciriaco@gmail.com>
2021-04-19 20:05:56 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ DEBUG = True
 # Secret key for session management. You can generate random strings here:
 # https://randomkeygen.com/
 SECRET_KEY = "W=L%c_w/,?w]eAeR'H-ytQQB4rk`{d5m0/UEtGrw`|-YS2*1%z2-(?l2Rs'K@xS"
-
-# Google Cloud Platform and Spotify API environment variables are defined here:
